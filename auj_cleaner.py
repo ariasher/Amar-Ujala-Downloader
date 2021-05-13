76fd4d7320c23fea0b94f9abeddfee39288e64d8
Mukul Khanna <mukulkhanna.96@gmail.com>
2021-05-13 20:22:02 +0530

    Added comments to Cleaner file.

--- auj_cleaner.py
+++ auj_cleaner.py
@@ -1,8 +1,25 @@
 import shutil
 
 class AUJCleaner():
+    '''
+    Cleaner class to remove all the folders and images created by the downloader.
+    '''
 
     def __init__(self, data, is_dict:False):
+        '''
+        Requires:
+            data[Dictonary/Tuple/List]: 
+              The data is an object of dictionary, tuple or list type which includes the 
+              directories which were created during the download of the newspaper.
+
+              If data is a type of dictionary with KEY(string dates) and VALUES(directories)
+              then the values are extracted by itself but the flag should be set to True.
+        
+        Optional: 
+            is_dict[Boolean]:
+              The flag to indicate whether data is a type of dictionary or not. Default: False.
+        '''
+        
         dirs = []
         
         if is_dict:
@@ -12,10 +29,17 @@ class AUJCleaner():
         
         self.data=dirs
 
-    def clean(self, verbose=False):
+    def clean(self, verbose:bool = False) -> None:
+        '''
+        This method is used to clean/remove the directories which were created during the download period.
+
+        Optional:
+            verbose[Boolean]:
+              This flag is used to show which directories have been created. Default: False.
+        '''
+
         for d in self.data:
             shutil.rmtree(d, ignore_errors=True)
             
             if verbose:
                 print(f"Directory deleted: {d}")
-
